21d6fade55e9ce7dfa35bb6d10520b63a5bbfd23
Theodore L'Heureux <theodorelheureux@gmail.com>
2023-02-18 13:46:24 -0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#212529",
+        secondary: "#343a40",
+        tertiary: "#495057",
+        quaternary: "#fb8500",
+        primaryText: "#8ecae6",
+        secondaryText: "#fdfcdc",
+      },
+    },
+  },
+  plugins: [],
+};
