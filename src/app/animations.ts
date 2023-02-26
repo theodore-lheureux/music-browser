8e523efb971302dfa2e84fda1d7a144557a0d8e3
Theodore L'Heureux <theodorelheureux@gmail.com>
2023-02-25 23:35:26 -0500

    style: make going from artist to index look like going back

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -9,7 +9,7 @@ import {
 } from '@angular/animations';
 
 export const routeAnimations = trigger('routeAnimations', [
-  transition('* => *', [
+  transition('index => artist', [
     query(
       ':enter, :leave',
       [
@@ -23,6 +23,31 @@ export const routeAnimations = trigger('routeAnimations', [
       { optional: true }
     ),
     query(':enter', [style({ left: '100%' })], { optional: true }),
+    query(':leave', animateChild(), { optional: true }),
+    group([
+      query(':leave', [animate('200ms ease-out', style({ left: '-100%' }))], {
+        optional: true,
+      }),
+      query(':enter', [animate('300ms ease-out', style({ left: '0%' }))], {
+        optional: true,
+      }),
+    ]),
+    query(':enter', animateChild(), { optional: true }),
+  ]),
+  transition('artist => index', [
+    query(
+      ':enter, :leave',
+      [
+        style({
+          position: 'absolute',
+          top: '5.5vh',
+          left: 0,
+          width: '100%',
+        }),
+      ],
+      { optional: true }
+    ),
+    query(':enter', [style({ left: '-100%' })], { optional: true }),
     query(':leave', animateChild(), { optional: true }),
     group([
       query(':leave', [animate('200ms ease-out', style({ left: '100%' }))], {
