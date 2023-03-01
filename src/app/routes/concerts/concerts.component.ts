46fb17806b46abe5b02abb00eb39e07c9fb87311
Theodore L'Heureux <theodorelheureux@gmail.com>
2023-03-01 14:01:10 -0500

    wip: show concert location

--- src/app/routes/concerts/concerts.component.ts
+++ src/app/routes/concerts/concerts.component.ts
@@ -1,11 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Artist } from 'src/app/models/artist.class';
+import { BandsintownService } from 'src/app/services/bandsintown.service';
+import { SearchService } from 'src/app/services/search.service';
+import { SpotifyService } from 'src/app/services/spotify.service';
+import { ConcertResponse } from '../../services/bandsintown.service';
+import { googleAPIKey } from 'src/app/app.config';
+
+const apiURL = 'https://www.google.com/maps/embed/v1/';
 
 @Component({
   selector: 'app-concerts',
   templateUrl: './concerts.component.html',
-  styles: [
-  ]
 })
-export class ConcertsComponent {
+export class ConcertsComponent implements OnInit, OnDestroy {
+  artistId: string;
+  artist: Artist | undefined;
+  concerts: ConcertResponse[] | undefined;
+  googleMapsURL: string | undefined;
 
+  constructor(
+    public spotify: SpotifyService,
+    public bandsintown: BandsintownService,
+    private search: SearchService,
+    private router: Router,
+    route: ActivatedRoute
+  ) {
+    this.artistId = route.snapshot.params['id'];
+
+    router.events.subscribe(() => {
+      const newId = route.snapshot.params['id'];
+      if (newId !== this.artistId) {
+        this.artistId = newId;
+        this.ngOnInit();
+      }
+    });
+  }
+
+  async ngOnInit(): Promise<void> {
+    this.artist = await this.spotify.getArtistById(this.artistId);
+
+    if (!this.artist) {
+      this.router.navigate(['/page-not-found']);
+      return;
+    }
+    document.title = 'Concerts of ' + this.artist.name + ' - Spotify';
+
+    this.search.setCurrentArtist(this.artist);
+
+    this.concerts = await this.bandsintown.getConcerts(this.artist.name);
+
+    this.googleMapsURL = `${apiURL}place?key=${googleAPIKey}&q=${this.concerts[0].venue.latitude},${this.concerts[0].venue.longitude}`;
+  }
+
+  ngOnDestroy(): void {
+    this.search.setCurrentArtist(undefined);
+  }
 }
--- src/app/services/bandsintown.service.ts
+++ src/app/services/bandsintown.service.ts
@@ -1,9 +1,37 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+import { bandsInTownAPIKey } from '../app.config';
+
+const apiURL = 'https://rest.bandsintown.com/';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BandsintownService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  async getConcerts(name: string): Promise<ConcertResponse[]> {
+    const url = `${apiURL}artists/${name}/events?app_id=${bandsInTownAPIKey}`;
+    const response = await lastValueFrom(this.http.get<ConcertResponse[]>(url));
+
+    return response;
+  }
+}
+
+export interface ConcertResponse {
+  id: string;
+  artist_id: string;
+  url: string;
+  on_sale_datetime: string;
+  datetime: string;
+  description: string;
+  venue: {
+    name: string;
+    country: string;
+    city: string;
+    region: string;
+    latitude: string;
+    longitude: string;
+  };
 }
